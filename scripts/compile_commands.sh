e1e8983169be69ea315964e45e235672378f4cc4
girlbuzz <minneelyyyy@proton.me>
2024-02-23 15:20:41 -0500

    fix: script should recognize alternate compilers

--- scripts/compile_commands.sh
+++ scripts/compile_commands.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 make --always-make --dry-run \
- | grep -wE 'gcc' \
+ | grep -wE 'cc|gcc|clang' \
  | jq -nR --arg wd "$PWD" '[inputs|{"directory": $wd, "command":., "file": match(" [^ ]+$").string[1:]}]' \
 > compile_commands.json
 
